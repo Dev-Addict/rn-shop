4bf43ac083b74f370125d1323a303ac37808fc3a
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-23 11:07:47 +0430

    create screens bases

---
+++ Screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ProductsOverviewScreen = () => {
+    return (
+        <View>
+            <Text>ProductsOverviewScreen</Text>
+        </View>
+    );
+};
+
+export default ProductsOverviewScreen;
