027c9e92ef0f5267bd3c3a77ab9a51f996d05a35
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-26 16:59:26 +0430

    create drawer navigation

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import {Ionicons} from "@expo/vector-icons";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import HeaderButton from '../components/HeaderButton'
@@ -10,32 +11,70 @@ import ProductsOverviewScreen from "../Screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../Screens/shop/ProductDetailScreen";
 import Colors from "../constants/Colors";
 import CartScreen from "../Screens/shop/CartScreen";
+import OrdersScreen from "../Screens/shop/OrdersScreen";
 
 const ShopStack = createStackNavigator();
+
+const Shop = () => (
+    <ShopStack.Navigator screenOptions={({navigation: {navigate, toggleDrawer}}) => ({
+        headerStyle: {
+            backgroundColor: Colors.darkerBackground,
+        },
+        headerTintColor: Colors.foreground,
+        headerTitleStyle: {
+            fontFamily: 'raleway'
+        },
+        headerBackTitle: '',
+        headerBackImage: () => <Ionicons name="md-arrow-back" size={25} color={Colors.foreground}/>,
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Cart" iconName="md-cart" onPress={() => {navigate('CartScreen')}}/>
+            </HeaderButtons>
+        ),
+        headerLeft: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Bars" iconName="md-menu" onPress={toggleDrawer}/>
+            </HeaderButtons>
+        )
+    })}>
+        <ShopStack.Screen name="ProductsOverview" component={ProductsOverviewScreen} options={{title: "Shop"}}/>
+        <ShopStack.Screen name="ProductDetailScreen" component={ProductDetailScreen}/>
+        <ShopStack.Screen name="CartScreen" component={CartScreen} options={{title: "Cart"}}/>
+    </ShopStack.Navigator>
+);
+
+const OrdersStack = createStackNavigator();
+
+const Orders = () => (
+    <OrdersStack.Navigator screenOptions={({navigation: {toggleDrawer}}) => ({
+        headerStyle: {
+            backgroundColor: Colors.darkerBackground,
+        },
+        headerTintColor: Colors.foreground,
+        headerTitleStyle: {
+            fontFamily: 'raleway'
+        },
+        headerBackTitle: '',
+        headerBackImage: () => <Ionicons name="md-arrow-back" size={25} color={Colors.foreground}/>,
+        headerLeft: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Bars" iconName="md-menu" onPress={toggleDrawer}/>
+            </HeaderButtons>
+        )
+    })}>
+        <OrdersStack.Screen name="OrdersScreen" component={OrdersScreen} title="Orders"/>
+    </OrdersStack.Navigator>
+);
+
+const ShopDrawer = createDrawerNavigator();
 
 const ShopNavigator = () => {
     return (
         <NavigationContainer>
-            <ShopStack.Navigator screenOptions={({navigation: {navigate}}) => ({
-                headerStyle: {
-                    backgroundColor: Colors.darkerBackground,
-                },
-                headerTintColor: Colors.foreground,
-                headerTitleStyle: {
-                    fontFamily: 'raleway'
-                },
-                headerBackTitle: '',
-                headerBackImage: () => <Ionicons name="md-arrow-back" size={25} color={Colors.foreground}/>,
-                headerRight: () => (
-                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                        <Item title="Cart" iconName="md-cart" onPress={() => {navigate('CartScreen')}}/>
-                    </HeaderButtons>
-                )
-            })}>
-                <ShopStack.Screen name="ProductsOverview" component={ProductsOverviewScreen} options={{title: "Shop"}}/>
-                <ShopStack.Screen name="ProductDetailScreen" component={ProductDetailScreen}/>
-                <ShopStack.Screen name="CartScreen" component={CartScreen} options={{title: "Cart"}}/>
-            </ShopStack.Navigator>
+            <ShopDrawer.Navigator initialRouteName="Shop">
+                <ShopDrawer.Screen name="Shop" component={Shop}/>
+                <ShopDrawer.Screen name="Orders" component={Orders}/>
+            </ShopDrawer.Navigator>
         </NavigationContainer>
     );
 };
