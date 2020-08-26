bbaf85a2b2a9fcaaaf6df28c66155d7066fd4799
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-26 16:34:55 +0430

    create order logic

--- Screens/shop/CartScreen.js
+++ Screens/shop/CartScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {FlatList, View} from 'react-native';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 import CartItem from "../../components/CartItem";
 import Text from "../../components/Text";
 import Button from "../../components/Button";
 import styles from "../../styles";
 import Colors from "../../constants/Colors";
+import {createOrder, emptyCart} from "../../actions";
+import Order from "../../models/Order";
 
 const CartScreen = ({navigation}) => {
+    const dispatch = useDispatch();
     const cart = useSelector(({cart}) => cart);
 
     const products = useSelector(({products}) => products).filter(({id}) => cart.includes(id));
@@ -31,7 +34,12 @@ const CartScreen = ({navigation}) => {
                 backgroundColor: Colors.darkerBackground
             }}>
                 <Text size={3}>Total: <Text style={{fontFamily: 'dancing-script'}} size={3}>{total}$</Text></Text>
-                <Button title="Order Now" style={total === 0 ? {opacity: 0.5} : {}}/>
+                <Button title="Order Now" style={total === 0 ? {opacity: 0.5} : {}} onPress={() => {
+                    if (total !== 0) {
+                        dispatch(createOrder(new Order(cart, total, '')));
+                        dispatch(emptyCart());
+                    }
+                }}/>
             </View>
         </View>
     );
--- models/Order.js
+++ models/Order.js
@@ -1,8 +1,6 @@
-import {v4 as uuidv4} from 'uuid'
-
 class Order {
     constructor(cart, total, address) {
-        this.id = uuidv4();
+        this.id = Date.now().toString(16);
         this.cart = cart;
         this.total = total;
         this.address = address;
